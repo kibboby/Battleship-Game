9fb887abe8cbb07b5d4944ca5b62a204c21455b5
Cheyenne <cheyennetan1999@hotmail.com>
2019-11-18 23:55:54 +0800

    change menu

--- src/MenuController.cs
+++ src/MenuController.cs
@@ -16,11 +16,11 @@ using SwinGameSDK;
 static class MenuController
 {
 
-	private const int MENU_TOP = 575;
+	private const int MENU_TOP = 530;
 	private const int MENU_LEFT = 30;
-	private const int MENU_GAP = 0;
+	private const int MENU_GAP = 10;
 	private const int BUTTON_WIDTH = 90;
-	private const int BUTTON_HEIGHT = 15;
+	private const int BUTTON_HEIGHT = 30;
 	private const int BUTTON_SEP = BUTTON_WIDTH + MENU_GAP;
 	private const int TEXT_OFFSET = 0;
 
@@ -99,7 +99,7 @@ static class MenuController
 	private const int OPTION_MENU_FULLSCREEN_BUTTON = 0;
 	private const int OPTION_MENU_BORDERLESS_BUTTON = 1;
 
-	private static readonly Color MENU_COLOR = SwinGame.RGBAColor(2, 167, 252, 255);
+	private static readonly Color MENU_COLOR = SwinGame.RGBAColor(255, 255, 255, 255);
 
 	private static readonly Color HIGHLIGHT_COLOR = SwinGame.RGBAColor(1, 57, 86, 255);
 	/// <summary>
