e4d26b3d939ecc354e6578cd716f62e9baacdbf1
DENIS SONG HUA YANG <100078285@students.swinburne.edu.my>
2019-11-21 15:04:15 +0800

    Test Select Battleship

---
+++ BattleShipTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SwinGameSDK;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace BattleShips
+{
+	[TestFixture()]
+	public class BattleShipTest
+	{
+		[Test()]
+		public void TestSelectedBattleShip ()
+		{
+			ShipName selectedBS = ShipName.Tug;
+			ShipName choose = default (ShipName);
+
+			if (choose == ShipName.None) {
+				selectedBS = choose;
+			}
+
+			Assert.AreEqual (choose, selectedBS);
+		}
+	}
+}
